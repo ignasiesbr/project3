654401eaf1ce3ab9406596e2c7945532ddff4fad
Ignasi Espinosa <ignasiespinosa@gmail.com>
2019-09-24 17:38:15 +0200

    cart partially working

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.datetime_safe import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+import decimal
 SIZES = (
     ("S", "SMALL"),
     ("L", "LARGE")
@@ -17,7 +17,7 @@ STYLES = (
 
 class Pasta(models.Model):
     name = models.CharField(max_length=64)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.name} - ${self.price}"
@@ -92,22 +92,21 @@ class Cart(models.Model):
     count = models.PositiveIntegerField(default=0)
     total = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
-"""
+
     def __str__(self):
         return "User: {} has {} items in their cart. Their total is ${}".format(self.user_id, self.count, self.total)
-"""
+
 class Entry(models.Model):
-    product = models.ForeignKey(Pasta, null=True, on_delete='CASCADE')
-    cart = models.ForeignKey(Cart, null=True, on_delete='CASCADE')
-    quantity = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey(Pasta, null=True, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, null=True, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return "This entry contains {} {}(s).".format(self.quantity, self.product.name)
-
+        return f"{self.product} - {self.quantity} units"
 
 @receiver(post_save, sender=Entry)
 def update_cart(sender, instance, **kwargs):
     line_cost = instance.quantity * instance.product.price
-    instance.cart.total += line_cost
+    instance.cart.total = decimal.Decimal(instance.cart.total) + line_cost
     instance.cart.count += instance.quantity
     instance.cart.updated = datetime.now()
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from orders.forms import PizzaForm
 from orders.models import *
 from django.db.models import F
+from django.db import models
+from django.urls import reverse
 # Create your views here.
 def index(request):
     form = PizzaForm()
+    my_cart, created = Cart.objects.get_or_create(user_id=request.user.id)
     context = {
         "pizzas":Pizza.objects.all(),
         "pastas":Pasta.objects.all(),
@@ -14,25 +17,27 @@ def index(request):
         "salads":Salad.objects.all(),
         "extrasubs":SubExtra.objects.all(),
         "form":form,
-        "user":request.user
+        "user":request.user,
+        "cart":my_cart,
+        "entries":Entry.objects.filter(cart=my_cart)
     }
     return render(request, "orders/index.html", context)
 
 def add_to_cart(request, pasta_id):
     pasta = Pasta.objects.get(id=pasta_id)
-    try:
-        my_cart = Cart.objects.get(user_id=request.user.id)
-    except Cart.DoesNotExist:
-        my_cart = Cart.objects.create(user_id=request.user.id)
-        my_cart.save()
+
+    my_cart, created = Cart.objects.get_or_create(user_id=request.user.id)
 
     try:
-        entry = Entry.objects.get(cart=my_cart)
-    except Entry.DoesNotExist:
+        cart_entries = Entry.objects.filter(cart=my_cart)
+        entry = cart_entries.get(product=pasta)
+        entry.quantity += 1
+        entry.save()
+        my_cart.total += pasta.price
+        my_cart.count += 1
+    except:
         entry = Entry.objects.create(product=pasta, cart=my_cart, quantity=1)
 
+    my_cart.save()
 
-        
-
-
-    return render(request, "orders/cart.html", )
+    return HttpResponseRedirect(reverse("index"))
